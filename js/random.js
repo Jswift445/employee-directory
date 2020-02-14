6294bcd1a058a0b9ca4c4b0ede00ba35806268cf
Ja-Juan Swift <swiftjajuan@gmail.com>
2020-02-14 14:42:23 -0600

    directory

--- js/random.js
+++ js/random.js
@@ -1,27 +1,26 @@
-const selectEmployee = document.getElementById('employees');
+const employeeList = document.getElementById('employees');
 
-
-
-//  $.ajax({
-//   url: 'https://randomuser.me/api/?inc=gender,name,location,email,picture',
-//   type: 'GET',
-//   dataType: 'json',
-//   success: function(res) {
-//     $('selectEmployee').html(res);
-//   }
-// });
 
 //Fetch API
-fetch('https://randomuser.me/api/?gender=female')
-  .then(response => response.json())
-  .then(data => console.log(data.picture))
+
+
+fetch('https://randomuser.me/api?results=12')
+        .then(response => response.json())
+        .then(data => generateHTML(data.results[0]))
+
 //
 
 //helper
-function generateImage(data) {
- const html = `
- <img src= '${data}' alt>
- `;
-  selectEmployee.innerHTML = html;
+function generateHTML(data, results) {
+  data.results.forEach(employees => {
+    let selectEmployee = document.createElement('div');
+    selectEmployee.setAttribute("class", "employee");
+    employeeList.appendChild(selectEmployee);
+    selectEmployee.innerHTML = `
+      <h2>${employees.name.first} ${employees.name.last}</h2>
+      <p>${employees.email}</p>
+      <p>${employees.location.city}</p>
+    `;
+  });
 }
 //
